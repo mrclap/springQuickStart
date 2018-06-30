c09ee9f56bc8ce31172e8bca70c0c211dec63d58
clap <mr.clap8@gmail.com>
2018-06-30 16:33:02 +0900

    springQuickStart 예제

---
+++ src/main/java/polymorphism/SamsungTV.java
@@ -0,0 +1,28 @@
+package polymorphism;
+
+public class SamsungTV implements TV{
+	public void initMethod() {
+		System.out.println("객체 초기화 작업 처리...");
+	}
+	
+	public void destoryMethod() {
+		System.out.println("객체 삭제 전 처리할 로직...");
+	}
+	
+	public SamsungTV() {
+		System.out.println("==> SamsungTV 객체 생성");
+	}
+	
+	public void powerOn() {
+		System.out.println("SamsungTV---전원을 켠다");
+	}
+	public void powerOff() {
+		System.out.println("SamsungTV---전원을 끈다");
+	}
+	public void volumeUp() {
+		System.out.println("SamsungTV---소리를 올린다");
+	}
+	public void volumeDown() {
+		System.out.println("SamsungTV---소리를 내린다");
+	}
+}
